7bb7db1900f558efd10363f87ef737d0f3e55142
Robin de Rooij <rderooij685@gmail.com>
2023-05-18 11:12:59 +0200

    Use different dbs instead of branches

--- openalex_terminusdb/insert.py
+++ openalex_terminusdb/insert.py
@@ -4,7 +4,12 @@ import subprocess
 import threading
 import os
 
-def init_db(schema):
+def init_db(schema, threads):
+    for x in range(0, threads):
+        try:
+            subprocess.run(f"./terminusdb db delete admin/openalex_{x}", shell=True)
+        except:
+            pass
     subprocess.run("./terminusdb db delete admin/openalex", shell=True)
     subprocess.run("./terminusdb db create admin/openalex", shell=True)
     subprocess.run(f"./terminusdb doc insert admin/openalex -g schema --full-replace < {schema}", shell=True)
@@ -12,9 +17,11 @@ def init_db(schema):
 def split_json(filename, threads):
     subprocess.run(f"split -n l/{threads} -d -a 1 {filename} openalex_split", shell=True)
 
-def ingest_json(filename, number):
-    subprocess.run(f'./terminusdb branch create admin/openalex/local/branch/openalex_{number}', shell=True)
-    subprocess.run(f'./terminusdb doc insert admin/openalex/local/branch/openalex_{number} < {filename}', shell=True)
+def ingest_json(filename, number, schema):
+    db = f'admin/openalex_{number}'
+    subprocess.run(f'./terminusdb db create {db}', shell=True)
+    subprocess.run(f"./terminusdb doc insert {db} -g schema --full-replace < {schema}", shell=True)
+    subprocess.run(f'./terminusdb doc insert {db} < {filename}', shell=True)
 
 def main():
     parser = argparse.ArgumentParser()
@@ -22,13 +29,14 @@ def main():
     parser.add_argument("schema", type=os.path.abspath)
     parser.add_argument("threads", type=int)
     args = parser.parse_args()
-    init_db(args.schema)
+    init_db(args.schema, args.threads)
     split_json(args.file, args.threads)
     for x in range(0, args.threads):
         number = str(x)
         print("RUNNING THREAD " + number)
-        t = threading.Thread(target=ingest_json, args=('openalex_split' + number, number))
+        t = threading.Thread(target=ingest_json, args=('openalex_split' + number, number, args.schema))
         t.start()
+    # TODO: We have to squash all the different data products into one
 
 
 if __name__ == '__main__':
