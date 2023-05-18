fe32f17fe2d3e02ab9b2abd2510b70f297792232
Robin de Rooij <rderooij685@gmail.com>
2023-05-18 11:19:00 +0200

    Make terminusdb command configurable

--- openalex_terminusdb/insert.py
+++ openalex_terminusdb/insert.py
@@ -4,24 +4,26 @@ import subprocess
 import threading
 import os
 
+TERMINUSDB_COMMAND = './terminusdb'
+
 def init_db(schema, threads):
     for x in range(0, threads):
         try:
-            subprocess.run(f"./terminusdb db delete admin/openalex_{x}", shell=True)
+            subprocess.run(f"{TERMINUSDB_COMMAND} db delete admin/openalex_{x}", shell=True)
         except:
             pass
-    subprocess.run("./terminusdb db delete admin/openalex", shell=True)
-    subprocess.run("./terminusdb db create admin/openalex", shell=True)
-    subprocess.run(f"./terminusdb doc insert admin/openalex -g schema --full-replace < {schema}", shell=True)
+    subprocess.run(f"{TERMINUSDB_COMMAND} db delete admin/openalex", shell=True)
+    subprocess.run(f"{TERMINUSDB_COMMAND} db create admin/openalex", shell=True)
+    subprocess.run(f"{TERMINUSDB_COMMAND} doc insert admin/openalex -g schema --full-replace < {schema}", shell=True)
 
 def split_json(filename, threads):
     subprocess.run(f"split -n l/{threads} -d -a 1 {filename} openalex_split", shell=True)
 
 def ingest_json(filename, number, schema):
     db = f'admin/openalex_{number}'
-    subprocess.run(f'./terminusdb db create {db}', shell=True)
-    subprocess.run(f"./terminusdb doc insert {db} -g schema --full-replace < {schema}", shell=True)
-    subprocess.run(f'./terminusdb doc insert {db} < {filename}', shell=True)
+    subprocess.run(f'{TERMINUSDB_COMMAND} db create {db}', shell=True)
+    subprocess.run(f"{TERMINUSDB_COMMAND} doc insert {db} -g schema --full-replace < {schema}", shell=True)
+    subprocess.run(f'{TERMINUSDB_COMMAND} doc insert {db} < {filename}', shell=True)
 
 def main():
     parser = argparse.ArgumentParser()
