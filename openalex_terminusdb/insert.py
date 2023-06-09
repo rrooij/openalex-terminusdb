af3f5d25525273bdd35b10ee2ed3273768cb9719
Robin de Rooij <rderooij685@gmail.com>
2023-06-09 14:59:12 +0200

    Threadpool changes

--- openalex_terminusdb/insert.py
+++ openalex_terminusdb/insert.py
@@ -4,12 +4,17 @@ import subprocess
 import threading
 import os
 import json
+import sys
+import time
+from multiprocessing import Pool
 
-TERMINUSDB_COMMAND = './terminusdb'
+TERMINUSDB_COMMAND = 'terminusdb'
 
 def prefix_number(number):
     str_number = str(number)
     if number < 10:
+        return "00" + str_number
+    elif number < 100:
         return "0" + str_number
     return str_number
 
@@ -17,11 +22,11 @@ def init_db(schema, threads):
     for x in range(0, threads):
         number = prefix_number(x)
         try:
-            subprocess.run(f"{TERMINUSDB_COMMAND} db delete admin/openalex_{number}", shell=True)
+#            subprocess.run(f"{TERMINUSDB_COMMAND} db delete admin/openalex_{number}", shell=True)
             subprocess.run(f'{TERMINUSDB_COMMAND} db create admin/openalex_{number}', shell=True)
         except:
             pass
-    subprocess.run(f"{TERMINUSDB_COMMAND} db delete admin/openalex", shell=True)
+#    subprocess.run(f"{TERMINUSDB_COMMAND} db delete admin/openalex", shell=True)
     subprocess.run(f"{TERMINUSDB_COMMAND} db create admin/openalex", shell=True)
     subprocess.run(f"{TERMINUSDB_COMMAND} doc insert admin/openalex -g schema --full-replace < {schema}", shell=True)
 
@@ -36,7 +41,11 @@ def add_types(filename):
                 f2.write("\n")
 
 def split_json(threads, filename):
-    subprocess.run(f"split -n l/{threads} -d -a 2 {filename} openalex_split", shell=True)
+    subprocess.run(f"split -n l/{threads} -d -a 3 {filename} openalex_split", shell=True)
+
+
+def split_db_json(threads, filename):
+    subprocess.run(f"split -n l/{threads} -d -a 3 {filename} dbs/db_split", shell=True)
 
 def apply_triples(threads):
     print("APPLYING TRIPPLES")
@@ -44,36 +53,47 @@ def apply_triples(threads):
         number = prefix_number(x)
         db_name = f"openalex_{number}"
         db = f'admin/{db_name}'
+        start = time.time()
         subprocess.run(f'{TERMINUSDB_COMMAND} triples load admin/openalex/local/branch/main/instance {db_name}.triples', shell=True)
-        os.remove(f'{db_name}.triples')
+        subprocess.run(f'sudo docker exec -i terminusdb /bin/bash -c \'rm -rf {db_name}.triples\'', shell=True)
 
-def ingest_json(filename, number, schema):
+def ingest_json(args):
+    start = time.time()
+    filename = args[0]
+    number = args[1]
+    schema = args[2]
     db_name = f"openalex_{number}"
     db = f'admin/{db_name}'
-    subprocess.run(f"{TERMINUSDB_COMMAND} doc insert {db} -g schema --full-replace < {schema}", shell=True)
-    subprocess.run(f'{TERMINUSDB_COMMAND} doc insert {db} < {filename}', shell=True)
-    subprocess.run(f'{TERMINUSDB_COMMAND} triples dump {db}/local/branch/main/instance > {db_name}.triples', shell=True)
+    with open(f"log/{db_name}.log", 'w') as f:
+        subprocess.run(f"{TERMINUSDB_COMMAND} doc insert {db} -g schema --full-replace < {schema}", shell=True, stdout=f, stderr=f)
+        subprocess.run(f'{TERMINUSDB_COMMAND} doc insert {db} < {filename}', shell=True, stdout=f, stderr=f)
+        end_insert = time.time() - start
+        f.write(f"\n\nEND TIME: {end_insert}\n")
+#    print(f"THREAD {number} finished inserting in: {end_insert} seconds")
+#    start = time.time()
+#    subprocess.run(f'sudo docker exec -i terminusdb /bin/bash -c \'./terminusdb triples dump {db}/local/branch/main/instance > {db_name}.triples\'', shell=True)
+#    end_triples = time.time() - start
+#    print(f"THREAD {number} dumped triples in: {end_triples} seconds")
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("file", type=os.path.abspath)
     parser.add_argument("schema", type=os.path.abspath)
+    parser.add_argument("split", type=int)
     parser.add_argument("threads", type=int)
     args = parser.parse_args()
 #    add_types(args.file)
-    init_db(args.schema, args.threads)
-    split_json(args.threads, args.file)
+    init_db(args.schema, args.split)
+#    split_json(args.split, args.file)
     threads = []
-    for x in range(0, args.threads):
-        number = prefix_number(x)
-        print("RUNNING THREAD " + number)
-        t = threading.Thread(target=ingest_json, args=('openalex_split' + number, number, args.schema))
-        threads.append(t)
-        t.start()
-    for thread in threads:
-        thread.join()
-    apply_triples(args.threads)
+    split_db_json(threads, 'dbs.list')
+    args_process = [('openalex_split' + prefix_number(x), prefix_number(x), args.schema) for x in range(0, args.split)]
+    with Pool(args.threads) as p:
+        # Ingest JSON
+        p.map(ingest_json, args_process)
+    print("FINISHED")
+#    apply_triples(args.threads)
     # TODO: We have to squash all the different data products into one
 
 
