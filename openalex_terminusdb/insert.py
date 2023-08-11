d51c1aacd6f4af1b77d91a7a0201aba8755ed4b9
Robin de Rooij <robin@terminusdb.com>
2023-08-11 17:44:21 +0200

    Use experimental -s flag and use concat feature

--- openalex_terminusdb/insert.py
+++ openalex_terminusdb/insert.py
@@ -22,13 +22,10 @@ def init_db(schema, threads):
     for x in range(0, threads):
         number = prefix_number(x)
         try:
-#            subprocess.run(f"{TERMINUSDB_COMMAND} db delete admin/openalex_{number}", shell=True)
             subprocess.run(f'{TERMINUSDB_COMMAND} db create admin/openalex_{number}', shell=True)
         except:
             pass
-#    subprocess.run(f"{TERMINUSDB_COMMAND} db delete admin/openalex", shell=True)
     subprocess.run(f"{TERMINUSDB_COMMAND} db create admin/openalex", shell=True)
-    subprocess.run(f"{TERMINUSDB_COMMAND} doc insert admin/openalex -g schema --full-replace < {schema}", shell=True)
 
 def add_types(filename):
     with open(filename, 'r') as f:
@@ -42,10 +39,6 @@ def add_types(filename):
 
 def split_json(threads, filename):
     subprocess.run(f"split -n l/{threads} -d -a 3 {filename} openalex_split", shell=True)
-
-
-def split_db_json(threads, filename):
-    subprocess.run(f"split -n l/{threads} -d -a 3 {filename} dbs/db_split", shell=True)
 
 def apply_triples(threads):
     print("APPLYING TRIPPLES")
@@ -66,7 +59,7 @@ def ingest_json(args):
     db = f'admin/{db_name}'
     with open(f"log/{db_name}.log", 'w') as f:
         subprocess.run(f"{TERMINUSDB_COMMAND} doc insert {db} -g schema --full-replace < {schema}", shell=True, stdout=f, stderr=f)
-        subprocess.run(f'{TERMINUSDB_COMMAND} doc insert {db} < {filename}', shell=True, stdout=f, stderr=f)
+        subprocess.run(f'{TERMINUSDB_COMMAND} doc insert {db} -s < {filename}', shell=True, stdout=f, stderr=f)
         end_insert = time.time() - start
         f.write(f"\n\nEND TIME: {end_insert}\n")
 #    print(f"THREAD {number} finished inserting in: {end_insert} seconds")
@@ -85,14 +78,24 @@ def main():
     args = parser.parse_args()
 #    add_types(args.file)
     init_db(args.schema, args.split)
-#    split_json(args.split, args.file)
+    split_json(args.split, args.file)
     threads = []
-    split_db_json(threads, 'dbs.list')
     args_process = [('openalex_split' + prefix_number(x), prefix_number(x), args.schema) for x in range(0, args.split)]
     with Pool(args.threads) as p:
-        # Ingest JSON
         p.map(ingest_json, args_process)
     print("FINISHED")
+    dbs = ""
+    for x in range(0,args.split):
+        dbs += f"admin/openalex_{x:03d}\n"
+    print("Merging database")
+    reset_output =subprocess.run(f'{TERMINUSDB_COMMAND} concat admin/openalex -j', text=True, shell=True, input=dbs, capture_output=True)
+    print(reset_output)
+    reset_id = reset_output.stdout.split('\n')[0]
+    print(reset_id)
+    subprocess.run(f'{TERMINUSDB_COMMAND} reset admin/openalex {reset_id}', text=True, shell=True)
+    print("Loading schema")
+#    subprocess.run(f'{TERMINUSDB_COMMAND} doc insert -f -g schema admin/openalex < ./new_schema.json', text=True, shell=True)
+#    subprocess.run(f'{TERMINUSDB_COMMAND} db update admin/openalex --schema=true', text=True, shell=True)
 #    apply_triples(args.threads)
     # TODO: We have to squash all the different data products into one
 
